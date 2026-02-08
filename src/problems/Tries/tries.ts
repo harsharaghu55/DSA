2aed21ed13e3cbc5bb4ee0663efc38efbbfedfbb
Harshavardhana Gowda M R <harshavardhanagowda99@outlook.com>
2026-02-08 22:04:13 +0530

    trie class created

--- src/problems/Tries/tries.ts
+++ src/problems/Tries/tries.ts
@@ -8,3 +8,38 @@
  * 70% uses cases around strings 30% about bits and other things
  * when you see strings and prefixes try thing about tries
  */
+
+type TrieData = string | null
+class TrieNode {
+    data:TrieData = 'a'
+    isEnd:boolean = false
+    children = new Map<string, TrieNode>()
+
+    constructor(data:TrieData = null){
+        this.data = data
+    }
+}
+
+class Trie{
+    root:TrieNode = new TrieNode()
+
+    insert(word:string){
+        let cur = this.root;
+        for(let char of word){
+            if(!cur.children.has(char)){
+                let newNode = new TrieNode(char)
+                cur.children.set(char,newNode)
+            }
+
+            cur = cur.children.get(char) as TrieNode
+        }
+        cur.isEnd = true
+    }
+}
+
+let trie = new Trie()
+trie.insert('cat')
+trie.insert('car')
+trie.insert('mat')
+trie.insert('cart')
+console.dir(trie, { depth: null, colors: true })
